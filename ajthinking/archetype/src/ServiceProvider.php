fbe5fd4c3682708fbd857028712ca115854f94a4
Anders Jurisoo <jurisoo@hotmail.com>
2022-03-09 15:36:51 +0100

    comment outre

--- ajthinking/archetype/src/ServiceProvider.php
+++ ajthinking/archetype/src/ServiceProvider.php
@@ -17,13 +17,13 @@ class ServiceProvider extends BaseServiceProvider
     {
         $this->registerFacades();
         $this->registerCommands();
-        $this->mergeConfigFrom(__DIR__.'/config/archetype.php', 'archetype');
+        // $this->mergeConfigFrom(__DIR__.'/config/archetype.php', 'archetype');
     }
 
     public function boot()
     {
-        $this->bootStrMacros(); 
-        $this->publishConfig();
+        // $this->bootStrMacros(); 
+        // $this->publishConfig();
     }
 
     protected function registerFacades()
